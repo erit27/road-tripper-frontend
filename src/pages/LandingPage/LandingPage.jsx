180621e53bfbffc4001b501d546496cdf456d362
Eilidh Ritchie <eilidh.ritchie@gmail.com>
2022-11-26 14:53:59 -0800

    setup folder structure

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import './LandingPage.scss';
+import PageHeader from '../../components/PageHeader/PageHeader';
+import BodyTemplate from '../../components/BodyTemplate/BodyTemplate';
+
+export default function LandingPage() {
+  return (
+    <>
+      <PageHeader />
+      <BodyTemplate />
+    </>
+  )
+}
