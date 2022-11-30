ac0877449fb4cf919b13037d94e678f81eca89c4
Eilidh Ritchie <eilidh.ritchie@gmail.com>
2022-11-30 12:32:52 -0800

    set up routing to main pages

---
+++ src/pages/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import './Gallery.scss';
+import PageHeader from '../../components/PageHeader/PageHeader';
+import BodyTemplate from '../../components/BodyTemplate/BodyTemplate';
+
+export default function Gallery() {
+  return (
+    <>
+      <PageHeader />
+      <BodyTemplate />
+    </>
+  )
+}
